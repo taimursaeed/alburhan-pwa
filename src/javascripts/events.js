1595967eab2f95c731833ac259ee535907b9bf54
Khawaja A <19771065+tmAsim@users.noreply.github.com>
2020-08-03 19:19:15 +0500

    User specific data implementation with selected date login implemented.

--- src/javascripts/events.js
+++ src/javascripts/events.js
@@ -6,33 +6,32 @@ async function mamulatFunction() {
   var _name = this.name;
   var _value = this.value;
   var date = window.timestamp;
-  var id = window.timestamp.toString();
-
+  var id = window.timestamp.toDateString();
+  var _userId = auth.currentUser.uid;
   notification.classList.remove('show');
 
   if (_name !== "") {
-    let document = await firebase.firestore().collection("mamulat").doc(id).get();
-    if (document && document.exists) {
-      await document.ref.update({
+    let document = await db.collection("mamulat").where("userId", "==", _userId).where("id", "==", id).get();
+    if (document && (document.docs.length > 0) && document.docs[0].data().userId == _userId) {
+      await document.docs[0].ref.update({
         [_name]: _value
       });
       notification.classList.add('show');
-      notificationTimer=setTimeout(() => {
+      notificationTimer = setTimeout(() => {
         notification.classList.remove('show');
       }, 3000);
     } else {
-      await document.ref.set({
+      await db.collection("mamulat").add({
         id: id,
         [_name]: _value,
-        date: date
-      }, {
-        merge: true
+        date: date,
+        userId: _userId
       });
       notification.classList.add('show');
-      notificationTimer=setTimeout(() => {
+      notificationTimer = setTimeout(() => {
         notification.classList.remove('show');
       }, 3000);
-     }
+    }
   }
 }
 
